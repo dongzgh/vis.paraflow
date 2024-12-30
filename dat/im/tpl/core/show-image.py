5e1e3fef49f68511a44ef9ad4b0c1a136de6a42c
Dong Zhao <dongzms@outlook.com>
2024-12-29 21:15:37 -0500

    Refactor image display to resize the image before showing it

--- dat/im/tpl/core/show-image.py
+++ dat/im/tpl/core/show-image.py
@@ -3,6 +3,7 @@ import cv2
 # Read image from disk.
 node_in_image = in_image
 cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Image', 600, 400)
-cv2.imshow('Image', node_in_image)
+resized_image = cv2.resize(node_in_image, (600, 400))
+cv2.imshow('Image', resized_image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
