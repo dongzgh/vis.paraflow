b79782f5e981d83f099986b271f565982d4c0579
Dong Zhao <dongzms@outlook.com>
2025-02-09 22:01:20 -0500

    Add image processing module with core functionalities and requirements

--- assets/video-processing/tpl/core/open-video.py
+++
@@ -1,5 +0,0 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
-
-# Load video file from disk.
-node_in_path = 'in_path'
-node_out_video = VideoFileClip(node_in_path)
--- assets/video-processing/tpl/process/extract-audio.py
+++
@@ -1,6 +0,0 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
-
-# Extract audio from video.
-node_in_video: VideoFileClip = in_video
-node_out_audio = node_in_video.audio
-node_out_video = node_in_video.without_audio()
