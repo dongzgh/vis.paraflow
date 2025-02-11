4400308e14ff510ed8f3f06ddab6687b55f5a27f
Dong Zhao <dongzms@outlook.com>
2025-02-11 14:31:28 -0500

    Add audio and video saving functionality with directory creation

--- video/tpl/core/save-audio.py
+++ video/tpl/core/save-audio.py
@@ -1,6 +1,9 @@
 from moviepy.audio.io.AudioFileClip import AudioFileClip
+import os
 
 # Save video to disk.
 node_in_audio: AudioFileClip = in_audio
 node_in_path = 'in_path'
+if not os.path.exists(os.path.dirname(node_in_path)):
+    os.makedirs(os.path.dirname(node_in_path))
 node_in_audio.write_audiofile(node_in_path)
