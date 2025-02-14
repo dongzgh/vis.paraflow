afd8a594ca7d927d9db3fc194ee0b60728645465
Dong Zhao <dongzms@outlook.com>
2025-02-14 14:52:58 -0500

    Update video output format to MP4, enhance requirements, and add macOS audio extraction test

--- dubbing/test/test-remove-audio-from-video-(macos).py
+++ dubbing/test/test-remove-audio-from-video-(macos).py
@@ -1,16 +1,19 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.audio.io.AudioFileClip import AudioFileClip
+import os
 
 # Load video file from disk.
-n088_in_path = '/Users/dong/Documents/github/visparaflow/assets/video/res/tutorial.mp4'
-n088_out_video = VideoFileClip(n088_in_path)
+n930_in_path = '/Users/dong/Documents/github/visParaflowAssets/dubbing/res/tutorial.mp4'
+n930_out_video = VideoFileClip(n930_in_path)
 
 # Extract audio from video.
-n462_in_video: VideoFileClip = n088_out_video
-n462_out_extracted_audio: AudioFileClip = n462_in_video.audio
-n462_out_extracted_video = n462_in_video.without_audio()
+n752_in_video: VideoFileClip = n930_out_video
+n752_out_extracted_audio: AudioFileClip = n752_in_video.audio
+n752_out_extracted_video = n752_in_video.without_audio()
 
 # Save video to disk.
-n542_in_video: VideoFileClip = n462_out_extracted_video
-n542_in_path = '/Users/dong/Documents/github/visparaflow/assets/video/out/result.mp4'
-n542_in_video.write_videofile(n542_in_path)
+n532_in_video: VideoFileClip = n752_out_extracted_video
+n532_in_path = '/Users/dong/Documents/github/visParaflowAssets/dubbing/out/result.mp4'
+if not os.path.exists(os.path.dirname(n532_in_path)):
+    os.makedirs(os.path.dirname(n532_in_path))
+n532_in_video.write_videofile(n532_in_path)
