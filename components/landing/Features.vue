39c6b61dc0b7bbe9f29cd9716a0dade6c2f65229
Dong Zhao <dongzms@outlook.com>
2025-02-19 14:22:56 -0500

    init commit

---
+++ components/landing/Features.vue
@@ -0,0 +1,65 @@
+<script setup>
+const features = [
+  {
+    title: "GUI Based Modeling",
+    description:
+      "Node based visual modeling tool. Drag, drop and connect nodes to create your own workflows. Save and load to reuse.",
+    icon: "bx:bxs-paint",
+  },
+  {
+    title: "Open Nodes Definition",
+    description:
+      "Define your own nodes and their templates. Use them in your workflows. Share them with others.",
+    icon: "bx:bxs-cube-alt",
+  },
+  {
+    title: "On Demand Node Sets",
+    description:
+      "Predefined node sets made available for common tasks. Customize and extend them to suit your own needs.",
+    icon: "bx:bxs-data",
+  },
+  {
+    title: "Cross Platform Supported",
+    description:
+      "Runs on Windows, macOS and Linux. Same user interface and same data. No need to worry about compatibility issues.",
+    icon: "bx:bxs-file-find",
+  },
+  {
+    title: "Automatic Script Generation",
+    description:
+      "One-click script generation. Python scripts is natively supported, but can be extended to other languages.",
+    icon: "bx:bxs-file-find",
+  },
+  {
+    title: "Easy Parametric Study",
+    description:
+      "No need to struggle with searching and modifying parameter values in scripts. What you see is what you get.",
+    icon: "bx:bxs-user",
+  },
+];
+</script>
+
+<template>
+  <div class="mt-16 md:mt-0">
+    <h2 class="text-4xl lg:text-5xl font-bold lg:tracking-tight">
+      Everything you need to start with workflow modeling
+    </h2>
+    <p class="text-lg mt-4 text-slate-600">
+      visParaflow comes with premade node sets to get you started. Customize and extend them to define your own node sets for your specific needs. 
+    </p>
+  </div>
+
+  <div class="grid sm:grid-cols-2 md:grid-cols-3 mt-16 gap-16">
+    <div v-for="item of features" class="flex gap-4 items-start">
+      <div class="mt-1 bg-black rounded-full p-2 w-8 h-8 shrink-0">
+        <Icon class="text-white" :name="item.icon" />
+      </div>
+      <div>
+        <h3 class="font-semibold text-lg">{{ item.title }}</h3>
+        <p class="text-slate-500 mt-2 leading-relaxed">
+          {{ item.description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/landing/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
+    <div class="p-12 md:order-1 hidden md:block">
+      <img
+        class="transition-transform transform hover:scale-110"
+        src="~/assets/img/hero.png"
+        alt="Starship starts the engine"
+        loading="eager"
+        format="avif"
+        width="800"
+        height="800"
+      />
+    </div>
+
+    <div>
+      <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight">
+        Model Workflows and Generate Scripts Made Easy
+      </h1>
+      <p class="text-lg mt-4 text-slate-600 max-w-xl">
+        visParaflow is a tool that helps you model workflows and generate scripts
+        for your projects. It bridges the gap between GUI based modeling and scripting.
+        It runs on Windows, macOS and Linux.
+      </p>
+      <div class="mt-6 flex flex-col sm:flex-row gap-3">
+        <LandingLink
+          href="https://github.com/dongzgh/vis.paraflow"
+          target="_blank"
+          rel="noopener"
+          >Github Repo</LandingLink
+        >
+      </div>
+    </div>
+  </main>
+</template>
