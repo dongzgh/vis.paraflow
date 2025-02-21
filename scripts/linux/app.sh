9b04f1d4b49bdef4fc5b0aa44eeadb7d58535806
Dong Zhao <dongzms@outlook.com>
2025-02-21 13:05:46 -0500

    Add packaging scripts and configuration for visParaflow application

--- scripts/linux/app.sh
+++ scripts/linux/app.sh
@@ -5,17 +5,13 @@ appname="visParaflow"
 # Set LD_LIBRARY_PATH.
 LD_LIBRARY_PATH=/opt/$appname
 export LD_LIBRARY_PATH
-echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
 # Set QT_PLUGIN_PATH.
 QT_PLUGIN_PATH=/opt/$appname/plugins
 export QT_PLUGIN_PATH
-echo "QT_PLUGIN_PATH=$QT_PLUGIN_PATH"
 
 # Set QT_QPA_PLATFORM to xcb or wayland.
 export QT_QPA_PLATFORM=wayland
-echo "QT_QPA_PLATFORM=$QT_QPA_PLATFORM"
 
 # Run the application.
-echo "Running application: /opt/$appname/bin/$appname"
 /opt/$appname/bin/$appname "$@"
---
+++ scripts/linux/make-app.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Set the working directory
+cd "$(dirname "$0")"
+
+# Set the package name
+appname="visParaflow"
+
+# Define the package directory
+pkgdir="app"
+
+# Delete the old package
+rm -rf $pkgdir/$appname
+echo "The old package directory has been deleted."
+
+# Create the destination directory
+mkdir -p $pkgdir/DEBIAN
+mkdir -p $pkgdir/opt/$appname/bin
+mkdir -p $pkgdir/opt/$appname/lib
+mkdir -p $pkgdir/opt/$appname/plugins
+mkdir -p $pkgdir/usr/share/applications
+mkdir -p $pkgdir/usr/share/icons/hicolor/48x48/apps
+echo "The destination package directory have been created."
+
+# Copy the DEBIAN control file.
+cp app.control $pkgdir/DEBIAN/control
+echo "The DEBIAN control file has been copied to package."
+
+# Copy the DEBIAN postinst file.
+cp app.postinst $pkgdir/DEBIAN/postinst
+echo "The DEBIAN postinst file has been copied to package."
+
+# Copy the DEBIAN postrm file.
+cp app.postrm $pkgdir/DEBIAN/postrm
+echo "The DEBIAN postrm file has been copied to package."
+
+# Copy the executable to the destination directory
+cp ../../build/release/bin/$appname $pkgdir/opt/$appname/bin/
+echo "The executable has been copied to the package."
+
+# Copy the Qt libraries to the destination directory
+cp -r /opt/Qt/6.8.1/gcc_64/lib $pkgdir/opt/$appname/
+echo "The dependency libraries have been copied to the package."
+
+# Copy the Qt plugins to the destination directory
+cp -r /opt/Qt/6.8.1/gcc_64/plugins/platforms $pkgdir/opt/$appname/plugins/
+echo "The dependency plugins have been copied to the package."
+
+# Copy the app script to the destination directory
+cp app.sh $pkgdir/opt/$appname/
+echo "The app script has been copied to the package."
+
+# Copy the app desktop file to the destination directory
+cp app.desktop $pkgdir/usr/share/applications/$appname.desktop
+echo "The app desktop file has been copied to the package."
+
+# Copy the app icon to the destination directory
+cp app.svg $pkgdir/usr/share/icons/hicolor/48x48/apps/$appname.svg
+echo "The app icon has been copied to the package."
+
+# Package the app.
+dpkg-deb --build $pkgdir $appname.deb
+echo "The DEBIAN package has been created."
+
+# Move the package to the deploy directory
+mkdir -p ../../deploy/linux
+rm -f ../../deploy/linux/$appname.deb
+mv $appname.deb ../../deploy/linux/
+echo "The DEBIAN package has been moved to the deploy directory."
+
+# Clean up the package directory
+rm -rf $pkgdir
+echo "The package directory has been cleaned up."
