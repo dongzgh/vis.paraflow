92fcf8a79ce663c4aa0425f3e7e619f71ef132ed
Dong Zhao <dongzms@outlook.com>
2025-02-28 13:56:44 -0500

    Remove unnecessary target properties from Windows build configuration

--- cmake/buildWindows.cmake
+++ cmake/buildWindows.cmake
@@ -14,10 +14,3 @@ add_executable(${CMAKE_PROJECT_NAME}
 
 # Set link libraries.
 target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE Qt6::Core Qt6::Widgets Qt6::Svg)
-
-# Set the target properties.
-set_target_properties(${CMAKE_PROJECT_NAME} PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIR}
-    LIBRARY_OUTPUT_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIR}
-    ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_ARCHIVE_OUTPUT_DIR} 
-)
