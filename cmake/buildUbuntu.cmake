2046cb0a98e0e8d97c02afe34130c7565e087078
Dong Zhao <dongzms@outlook.com>
2025-03-03 10:52:14 -0500

    Initialize page branch

--- cmake/buildUbuntu.cmake
+++
@@ -1,9 +0,0 @@
-# Add the executable target.
-add_executable(${CMAKE_PROJECT_NAME}
-    ${Implementation_FILES}
-    ${Header_FILES}
-    ${RESOURCE_FILE}
-)
-
-# Set link libraries.
-target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE Qt6::Core Qt6::Widgets Qt6::Svg)
